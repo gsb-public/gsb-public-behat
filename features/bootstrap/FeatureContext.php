68de159d5f63d9dc6040044da783b79a2470a144
Adam Moore <redndahead@gmail.com>
2015-04-14 10:16:59 -0700

    Add assertion. Configure behat.yml

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,9 +1,11 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 
 /**
  * Defines application features from the specific context.
@@ -20,4 +22,27 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
   public function __construct() {
   }
 
+  /** @BeforeScenario */
+  public function gatherContexts(BeforeScenarioScope $scope)
+  {
+    $environment = $scope->getEnvironment();
+
+    $this->minkContext = $environment->getContext('Drupal\DrupalExtension\Context\MinkContext');
+  }
+
+
+  /**
+   * Checks text in the page in the form:
+   * | text     |
+   * | Text 1   |
+   * | Text 2   |
+   *
+   * @Then I (should )see these texts:
+   */
+  public function assertTextsVisible(TableNode $texts) {
+    foreach ($texts->getHash() as $text) {
+      // Use the Mink Extension step definition.
+      $this->minkContext->assertPageContainsText($text['text']);
+    }
+  }
 }
